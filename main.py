e6b50751ee44b26d918804529d655e82950b1c5c
Ghostals <ghostals057@gmail.com>
2026-09-03 22:39:42 +0100

    Added desktop notifications
    Added desktop notification for screenshot capture and improved user feedback.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import win32clipboard
 import io
 import os
 from datetime import datetime
+from windows_toasts import Toast, WindowsToaster
 
 # directories
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +13,17 @@ CAPTURES_DIR = os.path.join(SCRIPT_DIR, "captures")
 # creates a directory for captures if it doesn't already exist
 os.makedirs(CAPTURES_DIR, exist_ok=True)
 
+# initialise desktop notification
+toaster = WindowsToaster("Python Screenshot Utility")
+toast = Toast()
+toast.text_fields = ["Copied Screenshot", "The screenshot has also been saved to your captures directory."]
 
+# define action on notification click
+def open_explorer(activated_args):
+    os.startfile(CAPTURES_DIR)
+
+
+# capture and copy a screenshot
 def capture_n_copy():
     print("Capturing Screenshot...")
 
@@ -24,6 +35,10 @@ def capture_n_copy():
     img.save(file_path)
     print(f"Saved a capture to {file_path}")
 
+    # send desktop notification
+    toast.on_actived = open_explorer
+    toaster.show_toast(toast)
+    
 
     # win clipboard expects device independent bitmap (DIB) format
     output = io.BytesIO()
